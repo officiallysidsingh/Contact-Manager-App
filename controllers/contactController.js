6ef552d11f879552a162f9fe849f4da22ac44194
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-16 19:57:11 +0530

    added error handling in server

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -17,6 +17,11 @@ const getContact = (req, res) => {
 //@access public
 const createContact = (req, res) => {
   console.log("The request body is: ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
   res.status(201).json({ message: "Create Contact" });
 };
 
