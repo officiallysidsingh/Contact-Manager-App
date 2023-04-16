2e40159936c33583007de5ee93dcf2854cc45ee8
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-16 20:52:05 +0530

    mongoDB and mongoose setup done

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
+connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
