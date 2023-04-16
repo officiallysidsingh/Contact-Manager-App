5c53281d5cd90caed762472f8d93b7ab20429ed6
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-16 21:07:00 +0530

    get contact by id from database

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -13,7 +13,12 @@ const getContacts = asyncHandler(async (req, res) => {
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get Contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found!");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Create new contact
