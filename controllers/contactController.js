55d98fb28769b53f51f72917a06162c544fa8ba4
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-17 15:52:49 +0530

    added function for deleting contact in mongodb

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -62,7 +62,15 @@ const updateContact = asyncHandler(async (req, res) => {
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found!");
+  }
+
+  await Contact.remove();
+
+  res.status(200).json(contact);
 });
 
 module.exports = {
