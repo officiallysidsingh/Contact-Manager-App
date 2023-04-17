63026bad8d449e974c94df7a222a64e1603647b9
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-17 16:31:06 +0530

    added user controller for functionality of each route and added register functionality successfully

---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+//@desc Register a user
+//@route POST /api/users/register
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
+
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hashing the Password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`User created: ${user}`);
+
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+//@desc Login a user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Login the user" });
+});
+
+//@desc Current user information
+//@route GET /api/users/current
+//@access private
+const currentUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Current user information" });
+});
+
+module.exports = { registerUser, loginUser, currentUser };
