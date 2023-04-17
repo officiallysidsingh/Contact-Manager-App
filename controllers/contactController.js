6ff122522eec4225e2a512e861eaf684a8372215
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-17 15:27:02 +0530

    added function for updating contact in mongodb

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -43,7 +43,19 @@ const createContact = asyncHandler(async (req, res) => {
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found!");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
