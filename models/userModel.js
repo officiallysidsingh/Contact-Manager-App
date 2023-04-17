6ed365463576e5ff7de6a9085ed7e739e03b0491
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-17 16:31:34 +0530

    added user mongoose schema

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please add the username"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add the user email address"],
+      unique: [true, "Email address already exists"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add the user password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
